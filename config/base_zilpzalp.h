f31525893e6b84151bbc067b75c1e81d25eefe63
yehorkhod <e.khodysko@gmail.com>
2026-01-14 19:53:58 +0200

    ZilpZalp

---
+++ config/base_zilpzalp.h
@@ -0,0 +1,83 @@
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+
+#define BASE 0
+#define L1 1
+#define L2 2
+
+// &lt {
+//     quick_tap_ms = <170>;
+// };
+
+&caps_word {
+    continue-list = <UNDERSCORE MINUS BSPC LSHFT RSHFT>;
+};
+
+#define COMBO(NAME, BINDINGS, KEYPOS) \
+combo_##NAME { \
+    timeout-ms = <20>; \
+    bindings = <BINDINGS>; \
+    key-positions = <KEYPOS>; \
+};
+
+/ {
+    combos {
+        compatible = "zmk,combos";
+        COMBO(q, &kp Q, 0 1)
+        COMBO(dqt, &kp DQT, 1 2)
+        COMBO(esc, &kp ESC, 8 9)
+        COMBO(z, &kp Z, 18 19)
+        COMBO(b, &kp B, 19 20)
+
+        COMBO(excl, &kp EXCL, 6 7)
+        COMBO(sqt, &kp SQT, 5 6)
+        COMBO(tab, &kp TAB, 16 17)
+        COMBO(qmark, &kp QMARK, 22 23)
+        COMBO(k, &kp K, 21 22)
+    };
+};
+
+/ {
+    behaviors {
+        ht: hold_tap {
+            compatible = "zmk,behavior-hold-tap";
+            label = "homerow mods left";
+            #binding-cells = <2>;
+            // flavor = "tap-preferred";
+            flavor = "balanced";
+            tapping-term-ms = <150>;
+            quick-tap-ms = <100>;
+            // global-quick-tap;
+            bindings = <&kp>, <&kp>;
+            // opposite side hand keys
+            hold-trigger-key-positions = <4 5 6 7 13 14 15 16 17 21 22 23 24 25 26 27>;
+        };
+        my_lt: my_layer_taps {
+            compatible = "zmk,behavior-hold-tap";
+            label = "my layer taps";
+            #binding-cells = <2>;
+            // flavor = "tap-preferred";
+            flavor = "balanced";
+            tapping-term-ms = <150>;
+            quick-tap-ms = <100>;
+            // global-quick-tap;
+            bindings = <&mo &kp>, <&kp>;
+            // non-thumb keys
+            // hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23>;
+        };
+        dotcol: dot_colon {
+            compatible = "zmk,behavior-mod-morph";
+            label = "DOT_COL";
+            #binding-cells = <0>;
+            bindings = <&kp DOT>, <&kp COLON>;
+            mods = <(MOD_LSFT|MOD_RSFT)>;
+        };
+        comsem: comma_semicolon {
+            compatible = "zmk,behavior-mod-morph";
+            label = "COM_SEM";
+            #binding-cells = <0>;
+            bindings = <&kp COMMA>, <&kp SEMI>;
+            mods = <(MOD_LSFT|MOD_RSFT)>;
+        };
+    };
+};
