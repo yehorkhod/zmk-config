7e892ad5928e1f0a51bcd7dd6a4525e6eafc0301
yehorkhod <e.khodysko@gmail.com>
2026-03-22 21:45:40 +0200

    feat: lt + vol/br + move ctrl✨

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -5,12 +5,14 @@
 #define L1 1
 #define L2 2
 
-// &lt {
-//     quick_tap_ms = <170>;
-// };
-
 &caps_word {
     continue-list = <UNDERSCORE MINUS BSPC LSHFT RSHFT>;
+};
+
+&lt {
+    flavor = "balanced";
+    tapping-term-ms = <150>;
+    quick-tap-ms = <100>;
 };
 
 #define COMBO(NAME, BINDINGS, KEYPOS) \
@@ -43,27 +45,11 @@ combo_##NAME { \
             compatible = "zmk,behavior-hold-tap";
             label = "homerow mods left";
             #binding-cells = <2>;
-            // flavor = "tap-preferred";
             flavor = "balanced";
             tapping-term-ms = <150>;
             quick-tap-ms = <100>;
-            // global-quick-tap;
             bindings = <&kp>, <&kp>;
-            // opposite side hand keys
             hold-trigger-key-positions = <4 5 6 7 13 14 15 16 17 21 22 23 24 25 26 27>;
-        };
-        my_lt: my_layer_taps {
-            compatible = "zmk,behavior-hold-tap";
-            label = "my layer taps";
-            #binding-cells = <2>;
-            // flavor = "tap-preferred";
-            flavor = "balanced";
-            tapping-term-ms = <150>;
-            quick-tap-ms = <100>;
-            // global-quick-tap;
-            bindings = <&mo &kp>, <&kp>;
-            // non-thumb keys
-            // hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23>;
         };
         dotcol: dot_colon {
             compatible = "zmk,behavior-mod-morph";
